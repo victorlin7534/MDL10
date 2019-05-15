ba0b490cb03fa67bd1e96e6016347e626862f582
root <root@localhost.localdomain>
2019-05-14 23:39:17 -0400

    3

--- gmath.py
+++ gmath.py
@@ -27,7 +27,7 @@ def get_lighting(normal, view, ambient, light, symbols, reflect ):
     normalize(n)
     normalize(light[LOCATION])
     normalize(view)
-    r = symbols['.white'][1]
+    r = symbols[reflect][1]
 
     a = calculate_ambient(ambient, r)
     d = calculate_diffuse(light, r, n)
